1645b81c1283194ff9139a0ed7a7bab696258ea4
ynksTT <159096223+ynksTT@users.noreply.github.com>
2024-05-17 11:53:13 +0900

    kadai_007
    kadai_007提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>kadai_007</title>
+</head>
+
+<body>
+  
+  <p>
+    <?php
+
+      $personal_data = ['name'=>'onion','price'=>200, 'weight' => 160];
+
+      print_r($personal_data);
+
+
+
+
+    ?>
+  </p>
+
+</body>
